04a20e64856c0a9c96c0847699f403391c7490c1
Cheri Allen <cherimarie@gmail.com>
2016-02-11 20:39:18 -0800

    Form submits nicely

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
 class StaticPagesController < ApplicationController
 
+  def about
+  end
+
+  def contact
+  end
+
   def submit_contact
-    flash[:notice] = "Thanks friend!"
+    puts params[:email]
+    puts params[:name]
+    puts params[:comments]
+    flash[:notice] = "Thanks #{params[:name]}!"
     redirect_to root_path
   end
 end
