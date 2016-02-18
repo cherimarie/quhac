cbc7d1f6367890f57a42f6459c9e741b77eb3ec5
Cheri Allen <cherimarie@gmail.com>
2016-02-17 18:39:11 -0800

    Add adv search

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+  get "/advanced_search", to: "static_pages#advanced_search"
   post "/submit_contact", to: "static_pages#submit_contact"
 
   root "static_pages#about"
