500e0d460d771c418e0c3b85e5738a86edd0dd4a
Cheri Allen <cherimarie@gmail.com>
2016-02-18 08:37:52 -0800

    Try to get rails admin to work on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.precompile += ["rails_admin/rails_admin.js", "rails_admin/rails_admin.css", "rails_admin/jquery.colorpicker.js", "rails_admin/jquery.colorpicker.css"]
   # Generate digests for assets URLs.
   config.assets.digest = true
 
