c6e052eeb75a8fd426badb668bd4d9924470f07d
Cheri Allen <cherimarie@gmail.com>
2016-03-13 14:10:26 -0700

    Updated specs & functionality

--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ProvidersController, type: :controller do
   before do
-    @pro1 = Provider.create(name: "Dr. McCoy", city: "Woodinville")
-    @pro2 = Provider.create(name: "Dr. Torres", city: "Seattle")
+    clinic1 = Clinic.create(street_address: "123 Pine St", city: "Seattle", zip: "98122", phone: "206-206-1206")
+    clinic2 = Clinic.create(street_address: "123 Spruce St", city: "Woodinville", zip: "98199", phone: "206-206-1206")
+    @pro1 = Provider.create(name: "Dr. McCoy", clinic_id: clinic2.id)
+    @pro2 = Provider.create(name: "Dr. Torres", clinic_id: clinic1.id)
+    @monkey_provider = Provider.create(name: "Dr. Monkey", clinic_id: clinic1.id)
+    @lion_provider = Provider.create(name: "Dr. Lion", clinic_id: clinic2.id)
   end
-  let!(:monkey_provider) { Provider.create(name: "Dr. Monkey", city: "Bellingham")}
-  let!(:lion_provider) { Provider.create(name: "Dr. Lion", city: "Seattle")}
 
 
   describe "index" do
@@ -16,21 +18,22 @@ RSpec.describe ProvidersController, type: :controller do
       expect(assigns(:providers).length).to eq(4)
       expect(assigns(:providers)).to include(@pro1)
       expect(assigns(:providers)).to include(@pro2)
-      expect(assigns(:providers)).to include(lion_provider)
-      expect(assigns(:providers)).to include(monkey_provider)
+      expect(assigns(:providers)).to include(@lion_provider)
+      expect(assigns(:providers)).to include(@monkey_provider)
     end
 
 
     it "returns providers that match query" do
       get :index, search: "monkey"
 
-      expect(assigns(:providers)).to eq([monkey_provider])
+      expect(assigns(:providers)).to eq([@monkey_provider])
     end
 
     it "returns providers that match query" do
+      pending "search does not hit city right now, as it's delegated to clinic"
       get :index, search: "Seattle"
 
-      expect(assigns(:providers)).to include(lion_provider)
+      expect(assigns(:providers)).to include(@lion_provider)
       expect(assigns(:providers)).to include(@pro2)
     end
   end
--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Provider, type: :model do
   describe "validations" do
-    it "requires a name and city" do
-      expect(Provider.new(name: "Dr. Feelgood")).to be_invalid
-      expect(Provider.new(city: "Funkytown")).to be_invalid
-      expect(Provider.new(name: "Dr. Feelgood", city: "Funkytown")).to be_valid
+    it "requires a name and clinic_id" do
+      clinic = Clinic.create(street_address: "123 Pine St", city: "Seattle", zip: "98122", phone: "206-206-1206")
+      expect(Provider.new(name: "Dr. Feelgood", clinic_id: nil)).to be_invalid
+      expect(Provider.new(name: nil, clinic_id: clinic.id)).to be_invalid
+      expect(Provider.new(name: "Dr. Feelgood", clinic_id: clinic.id)).to be_valid
     end
   end
 
   describe "associations" do
     before do
+      @clinic = Clinic.create(street_address: "123 Pine St", city: "Seattle", zip: "98122", phone: "206-206-1206")
       @ins = Insurer.create(name: "Blue Cross")
-      @pro = Provider.create(name: "Dr. Feelgood", city: "Funkytown")
+      @pro = Provider.create(name: "Dr. Feelgood", clinic_id: @clinic.id)
       @prov_ins = ProviderInsurer.create(provider_id: @pro.id, insurer_id: @ins.id)
     end
     it "has many provider_insurers" do
@@ -21,6 +23,37 @@ RSpec.describe Provider, type: :model do
 
     it "has many insurers" do
       expect(@pro.insurers).to eq([@ins])
+    end
+
+    it "belongs to a clinic" do
+      expect(@pro.clinic).to eq(@clinic)
+    end
+  end
+
+  describe "delegations" do
+    before do
+      @clinic = Clinic.create(street_address: "123 Pine St", city: "Seattle", zip: "98122", phone: "206-206-1206")
+      @pro = Provider.create(name: "Dr. Feelgood", clinic_id: @clinic.id)
+    end
+
+    it "delegates street_address to clinic" do
+      expect(@pro.street_address).to eq(@clinic.street_address)
+    end
+
+    it "delegates city to clinic" do
+      expect(@pro.city).to eq(@clinic.city)
+    end
+
+    it "delegates zip to clinic" do
+      expect(@pro.zip).to eq(@clinic.zip)
+    end
+
+    it "delegates phone to clinic" do
+      expect(@pro.phone).to eq(@clinic.phone)
+    end
+
+    it "delegates website to clinic" do
+      expect(@pro.website).to eq(@clinic.website)
     end
   end
 end
