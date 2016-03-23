460ebc5ba7a975504c2bcc531a3f68f757b0c940
Cheri Allen <cherimarie@gmail.com>
2016-03-22 21:45:01 -0700

    More helpers, clean up views

--- app/helpers/filter_form_helper.rb
+++ app/helpers/filter_form_helper.rb
@@ -6,4 +6,16 @@ module FilterFormHelper
   def specializations
     Provider.all.map{ |p| p.specialization }.compact.uniq.reject{ |el| el.empty? }
   end
+
+  def gender_identities
+    Provider.all.map{ |p| p.gender_id }.compact.uniq.reject{ |el| el.empty? }
+  end
+
+  def orientations
+    Provider.all.map{ |p| p.orientation }.compact.uniq.reject{ |el| el.empty? }
+  end
+
+  def prov_types
+    Provider.all.map{ |p| p.type }.compact.uniq.reject{ |el| el.empty? }
+  end
 end
