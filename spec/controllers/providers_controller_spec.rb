5319b656cc621f652d29e2b3451dc29dc3d4aafe
Cheri Allen <cherimarie@gmail.com>
2016-04-10 16:59:46 -0700

    Tests for text search

--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -17,30 +17,98 @@ RSpec.describe ProvidersController, type: :controller do
       phone: "206-206-1206"
     )
 
-    @pro2 = Provider.create(
+    @bull_pro = Provider.create(
       name: "Dr. Torres",
       clinic_id: clinic1.id,
-      specialization: "diabetics, squids",
-      type: "phrenologist"
+      specialization: "diabetics, squids, pediatric oncology",
+      type: "phrenologist",
+      email: nil,
+      population_expertise: "children",
+      gender_id: "male",
+      orientation: nil,
+      comprehensive_intake: nil,
+      use_pref_name: nil,
+      trans_standard_of_care: nil,
+      gender_neutral_rr: nil,
+      lgbtq_trained: nil,
+      lgbtq_training_details: nil,
+      cultural_trained: nil,
+      cultural_training_details: nil,
+      lgbq_incl_strategy: nil,
+      trans_incl_strategy: "Listen to people's choices and identities",
+      new_clients: nil,
+      community_relationship: nil,
+      additional: "Donate to Camp Ten Trees every year"
     )
-    @monkey_provider = Provider.create(
+    @monkey_pro = Provider.create(
       name: "Dr. Monkey",
       clinic_id: clinic1.id,
       specialization: "trans-men, athletes",
-      type: "dermatologist"
+      type: "dermatologist",
+      email: nil,
+      population_expertise: nil,
+      gender_id: "female",
+      orientation: "straight",
+      comprehensive_intake: nil,
+      use_pref_name: nil,
+      trans_standard_of_care: nil,
+      gender_neutral_rr: nil,
+      lgbtq_trained: nil,
+      lgbtq_training_details: "Edna, in accounting, said to be cool",
+      cultural_trained: nil,
+      cultural_training_details: "We read a lot",
+      lgbq_incl_strategy: nil,
+      trans_incl_strategy: nil,
+      new_clients: nil,
+      community_relationship: nil,
+      additional: "I want to hug everyone"
     )
 
-    @pro1 = Provider.create(
+    @trek_pro = Provider.create(
       name: "Dr. McCoy",
       clinic_id: @clinic2.id,
       specialization: "trans-men, diabetics, athletes",
-      type: ""
+      type: "",
+      email: nil,
+      population_expertise: "Vulcans, Worf",
+      gender_id: nil,
+      orientation: "it's complicated",
+      comprehensive_intake: nil,
+      use_pref_name: nil,
+      trans_standard_of_care: nil,
+      gender_neutral_rr: nil,
+      lgbtq_trained: nil,
+      lgbtq_training_details: "Workshop from Bob Ross",
+      cultural_trained: nil,
+      cultural_training_details: "Workshop by Bobby Blass",
+      lgbq_incl_strategy: nil,
+      trans_incl_strategy: nil,
+      new_clients: nil,
+      community_relationship: "Sponsor Gay City's softball team",
+      additional: ""
     )
-    @lion_provider = Provider.create(
+    @lion_pro = Provider.create(
       name: "Dr. Lion",
       clinic_id: @clinic2.id,
       specialization: "",
-      type: "acupuncturist"
+      type: "acupuncturist",
+      email: "rawr@example.com",
+      population_expertise: "children",
+      gender_id: "two spirit",
+      orientation: "gay",
+      comprehensive_intake: nil,
+      use_pref_name: nil,
+      trans_standard_of_care: nil,
+      gender_neutral_rr: nil,
+      lgbtq_trained: nil,
+      lgbtq_training_details: "Cheryl Ingram taught workshop",
+      cultural_trained: nil,
+      cultural_training_details: nil,
+      lgbq_incl_strategy: "Respect for all clients",
+      trans_incl_strategy: nil,
+      new_clients: nil,
+      community_relationship: nil,
+      additional: "I like turtles"
     )
   end
 
@@ -50,10 +118,10 @@ RSpec.describe ProvidersController, type: :controller do
       get :index
 
       expect(assigns(:providers).length).to eq(4)
-      expect(assigns(:providers)).to include(@pro1)
-      expect(assigns(:providers)).to include(@pro2)
-      expect(assigns(:providers)).to include(@lion_provider)
-      expect(assigns(:providers)).to include(@monkey_provider)
+      expect(assigns(:providers)).to include(@trek_pro)
+      expect(assigns(:providers)).to include(@bull_pro)
+      expect(assigns(:providers)).to include(@lion_pro)
+      expect(assigns(:providers)).to include(@monkey_pro)
     end
   end
 
@@ -61,21 +129,21 @@ RSpec.describe ProvidersController, type: :controller do
     it "returns providers whose name matches query" do
       get :index, search: "monkey"
 
-      expect(assigns(:providers)).to eq([@monkey_provider])
+      expect(assigns(:providers)).to eq([@monkey_pro])
     end
 
     it "returns providers whose type matches query" do
       get :index, search: "phrenologist"
 
-      expect(assigns(:providers)).to eq([@pro2])
+      expect(assigns(:providers)).to eq([@bull_pro])
     end
 
     it "returns providers whose specialization matches query" do
       get :index, search: "trans-men"
 
       expect(assigns(:providers).length).to eq(2)
-      expect(assigns(:providers)).to include(@monkey_provider)
-      expect(assigns(:providers)).to include(@pro1)
+      expect(assigns(:providers)).to include(@monkey_pro)
+      expect(assigns(:providers)).to include(@trek_pro)
     end
 
     it "returns providers whose clinic name matches query" do
@@ -83,13 +151,85 @@ RSpec.describe ProvidersController, type: :controller do
 
       expect(assigns(:providers).length).to eq(2)
       expect(assigns(:clinics)).to eq([@clinic2])
-      expect(assigns(:providers)).to include(@lion_provider)
-      expect(assigns(:providers)).to include(@pro1)
+      expect(assigns(:providers)).to include(@lion_pro)
+      expect(assigns(:providers)).to include(@trek_pro)
     end
   end
 
   describe "text search on index" do
+    it "searches name" do
+      get :index, text_search: "Lion"
 
+      expect(assigns(:providers)).to eq([@lion_pro])
+    end
+
+    it "searches email" do
+      get :index, text_search: "rawr@example.com"
+
+      expect(assigns(:providers)).to eq([@lion_pro])
+    end
+    it "searches type" do
+      get :index, text_search: "acupuncturist"
+
+      expect(assigns(:providers)).to eq([@lion_pro])
+    end
+    it "searches population_expertise" do
+      get :index, text_search: "Vulcans"
+
+      expect(assigns(:providers)).to eq([@trek_pro])
+    end
+    it "searches specialization" do
+      get :index, text_search: "pediatric oncology"
+
+      expect(assigns(:providers)).to eq([@bull_pro])
+    end
+    it "searches gender_id" do
+      # is this a problem?
+      get :index, text_search: "male"
+
+      expect(assigns(:providers)).to eq([@bull_pro, @monkey_pro])
+    end
+    it "searches orientation" do
+      get :index, text_search: "straight"
+
+      expect(assigns(:providers)).to eq([@monkey_pro])
+    end
+    it "searches trans_standard_of_care" do
+      # pending "this is maybe not right"
+      # get :index, text_search: "wpath"
+
+      # expect(assigns(:providers)).to eq([])
+    end
+    it "searches lgbtq_training_details" do
+      get :index, text_search: "Cheryl Ingram"
+
+      expect(assigns(:providers)).to eq([@lion_pro])
+    end
+    it "searches cultural_training_details" do
+      get :index, text_search: "bobby"
+
+      expect(assigns(:providers)).to eq([@trek_pro])
+    end
+    it "searches lgbq_incl_strategy" do
+      get :index, text_search: "respect"
+
+      expect(assigns(:providers)).to eq([@lion_pro])
+    end
+    it "searches trans_incl_strategy" do
+      get :index, text_search: "identities"
+
+      expect(assigns(:providers)).to eq([@bull_pro])
+    end
+    it "searches community_relationship" do
+      get :index, text_search: "softball"
+
+      expect(assigns(:providers)).to eq([@trek_pro])
+    end
+    it "searches additional" do
+      get :index, text_search: "hug"
+
+      expect(assigns(:providers)).to eq([@monkey_pro])
+    end
   end
 
   describe "filter search on index" do
@@ -98,9 +238,9 @@ RSpec.describe ProvidersController, type: :controller do
 
   describe "show" do
     it "sets the provider correctly" do
-      get :show, id: @pro1.id
+      get :show, id: @trek_pro.id
 
-      expect(assigns(:provider)).to eq(@pro1)
+      expect(assigns(:provider)).to eq(@trek_pro)
     end
   end
 end
