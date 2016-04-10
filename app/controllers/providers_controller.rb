973b4415f37df27efc85427fd87698b253539fac
Cheri Allen <cherimarie@gmail.com>
2016-04-09 17:12:01 -0700

    Quick search searches on desired attrs of prov & clinic

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -4,6 +4,8 @@ class ProvidersController < ApplicationController
   def index
     if params[:search]
       @providers = Provider.search(params[:search])
+      @clinics = Clinic.search(params[:search])
+      @providers += Provider.where(clinic: @clinics)
       @showing_results = true
     else
       @providers = Provider.all
--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -2,12 +2,46 @@ require 'rails_helper'
 
 RSpec.describe ProvidersController, type: :controller do
   before do
-    clinic1 = Clinic.create(street_address: "123 Pine St", city: "Seattle", zip: "98122", phone: "206-206-1206")
-    clinic2 = Clinic.create(street_address: "123 Spruce St", city: "Woodinville", zip: "98199", phone: "206-206-1206")
-    @pro1 = Provider.create(name: "Dr. McCoy", clinic_id: clinic2.id)
-    @pro2 = Provider.create(name: "Dr. Torres", clinic_id: clinic1.id)
-    @monkey_provider = Provider.create(name: "Dr. Monkey", clinic_id: clinic1.id)
-    @lion_provider = Provider.create(name: "Dr. Lion", clinic_id: clinic2.id)
+    clinic1 = Clinic.create(
+      name: "Jungle Clinic",
+      street_address: "123 Pine St",
+      city: "Seattle",
+      zip: "98122",
+      phone: "206-206-1206"
+    )
+    @clinic2 = Clinic.create(
+      name: "Savannah Clinic",
+      street_address: "123 Spruce St",
+      city: "Woodinville",
+      zip: "98199",
+      phone: "206-206-1206"
+    )
+
+    @pro2 = Provider.create(
+      name: "Dr. Torres",
+      clinic_id: clinic1.id,
+      specialization: "diabetics, squids",
+      type: "phrenologist"
+    )
+    @monkey_provider = Provider.create(
+      name: "Dr. Monkey",
+      clinic_id: clinic1.id,
+      specialization: "trans-men, athletes",
+      type: "dermatologist"
+    )
+
+    @pro1 = Provider.create(
+      name: "Dr. McCoy",
+      clinic_id: @clinic2.id,
+      specialization: "trans-men, diabetics, athletes",
+      type: ""
+    )
+    @lion_provider = Provider.create(
+      name: "Dr. Lion",
+      clinic_id: @clinic2.id,
+      specialization: "",
+      type: "acupuncturist"
+    )
   end
 
 
@@ -23,18 +57,33 @@ RSpec.describe ProvidersController, type: :controller do
     end
 
 
-    it "returns providers that match query" do
+    it "returns providers whose name matches query" do
       get :index, search: "monkey"
 
       expect(assigns(:providers)).to eq([@monkey_provider])
     end
 
-    it "returns providers that match query" do
-      pending "search does not hit city right now, as it's delegated to clinic"
-      get :index, search: "Seattle"
+    it "returns providers whose type matches query" do
+      get :index, search: "phrenologist"
 
+      expect(assigns(:providers)).to eq([@pro2])
+    end
+
+    it "returns providers whose specialization matches query" do
+      get :index, search: "trans-men"
+
+      expect(assigns(:providers).length).to eq(2)
+      expect(assigns(:providers)).to include(@monkey_provider)
+      expect(assigns(:providers)).to include(@pro1)
+    end
+
+    it "returns providers whose clinic name matches query" do
+      get :index, search: "Savannah"
+
+      expect(assigns(:providers).length).to eq(2)
+      expect(assigns(:clinics)).to eq([@clinic2])
       expect(assigns(:providers)).to include(@lion_provider)
-      expect(assigns(:providers)).to include(@pro2)
+      expect(assigns(:providers)).to include(@pro1)
     end
   end
 
