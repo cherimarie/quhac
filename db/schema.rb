f4cf2c3ca2555c05c04bbfd0f3f874da8e75767e
Cheri Allen <cherimarie@gmail.com>
2016-04-19 20:19:15 -0700

    Add icon to providers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313201315) do
+ActiveRecord::Schema.define(version: 20160420031826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20160313201315) do
     t.boolean  "new_clients"
     t.text     "community_relationship"
     t.text     "additional"
+    t.string   "icon"
   end
 
   create_table "users", force: true do |t|
